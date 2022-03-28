4fe100ecd2938257c9f1dd520b51c5e670429ef2
yura-169 <102508561+yura-169@users.noreply.github.com>
2022-03-28 16:06:25 +0900

    Create viewCircularSector.m

---
+++ viewCircularSector.m
@@ -0,0 +1,9 @@
+function [x,y,inputVal]=viewCircularSector_(inputVal)
+%input form : range*azimuth
+%output : surf(x,y,heatmap)
+num_range = size(inputVal,1);
+num_theta = size(inputVal,2);
+startTheta = (180 - num_theta)/2;
+[x,y,z] = sph2cart(repmat(deg2rad(startTheta+1:180-startTheta),[num_range 1]),zeros(num_range, num_theta),repmat((1:num_range)',[1 num_theta]));
+surf(x,y,abs(inputVal)); view(2);
+end
